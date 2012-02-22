b3a49bbb7bb5605e8e9a3e71d734e2dda78b4a11
babyrabbit <kittyliao7_tw@yahoo.com>
2012-02-22 10:05:29 +0100

    21.feb. 2012
    Signed-off-by: babyrabbit <kittyliao7_tw@yahoo.com>
    
    Signed-off-by: babyrabbit <kittyliao7_tw@yahoo.com>

--- tmap.c
+++ tmap.c
@@ -1,19 +1,39 @@
 #define TEST 0
 #define CALIBRATION 0
-#define MEASURE 1
+#define
 
 sbit AB0 = chans ^ 0;		// flag for AB 0
 sbit AB1 = chans ^ 1;		// flag for AB 1
-sbit 
-sbit source0 = P0 ^ 0;		//control current source 0
-sbit source1 = P0 ^ 1;		//control current source 1
+sbit AB2 =
+sbit AB3 = 
+ 
+sbit current0 = P0 ^ 0;		//control current source 0
+sbit current1 = P0 ^ 1;		//control current source 1
+sbit current2 = P3 ^ 7;		//control current source 2
+sbit current3 = P3 ^ 6;		//control current source 3
 
-sbit int0 = P2 ^ 0;			//temp AB1 OVER
-sbit int1 = P2 ^ 1;			//temp AB2 OVER
+sbit MUXIN0 = P0 ^ 5;		//control multiplexer 0 selection pins 
+sbit MUXA0 = P0 ^ 4;  
+sbit MUXB0 = P0 ^ 3;
+sbit MUXC0 = P0 ^ 2;
+
+sbit MUXIN1 = P2 ^7 ;
+sbit MUXA1 = P2 ^ 6;
+sbit MUXB1 = P2 ^ 5;
+sbit MUXC1 = P2 ^ 4;
+
+sbit MUXIN2 = P2 ^3 ;
+sbit MUXA2 = P2 ^ 2;
+sbit MUXB2 = P2 ^ 1;
+sbit MUXC2 = P2 ^ 0;
+
+sbit MUXIN3 = P3 ^ 5;
+sbit MUXA3 = P3 ^ 4;
+sbit MUXB3 = P3 ^ 3;
+sbit MUXC3 = P3 ^ 2;
+
 sfr16 EDAT1 = 0x0BC;		//edata1=low edata2=high byte
 sfr16 ADC = 0x0D9;
-sfr16 DAC0= 0x0F9;
-sfr16 DAC1=0x0FB;
 sfr16 EDA1= 0xBC;
 
 int enable_source(int source, int onoff){
@@ -57,14 +77,14 @@ void main(void){
 	init_adc();  // initialise ADC
 	enable_current(); //enable the current source
 	get_temp(); //load temperature from the lakeshore thermometer
-	
 	read_current(); //read current value
 	check_current(); // check if 5uA current value is wrong
+	while (1){
 	select_sensor(); //start sensor selection
 	read_voltage(); // read voltage across this sensor
 	
 	save_data(); //put V[i] I[i] into a memory 
-	write_data();
+	}
 	
-	do_temperature();
+	
 	}
